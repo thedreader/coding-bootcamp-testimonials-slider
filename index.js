77e19bd34d9f6d9dc11a04a3c58706cd49276ecd
thedreader <70570939+thedreader@users.noreply.github.com>
2020-11-17 01:15:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+$(".next").click(function (){
+
+  $(".slide-1").fadeToggle().toggleClass("hide");
+  $(".slide-2").fadeToggle().toggleClass("hide");
+
+});
+
+$(".prev").click(function (){
+
+  $(".slide-1").fadeToggle().toggleClass("hide");
+  $(".slide-2").fadeToggle().toggleClass("hide");
+});
